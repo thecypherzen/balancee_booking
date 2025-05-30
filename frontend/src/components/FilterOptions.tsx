d0934586d68801168fc98b182206d3a905381391
William Inyam <sw.inyam@outlook.com>
2025-05-30 00:20:33 +0000

    :construction:wip:add car type filter + improve ui

--- frontend/src/components/FilterOptions.tsx
+++ frontend/src/components/FilterOptions.tsx
@@ -1,10 +1,12 @@
 import { Checkbox } from "@/components/ui/checkbox";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 type ServicesProps = {
   services: string[];
   setServices: React.Dispatch<React.SetStateAction<string[]>>;
 };
 
+// Services
 const ServiceOptions: React.FC<ServicesProps> = ({ services, setServices }) => {
   const servicesList = [
     "Oil Change",
@@ -27,7 +29,7 @@ const ServiceOptions: React.FC<ServicesProps> = ({ services, setServices }) => {
   };
 
   return (
-    <div className="flex flex-col items-center border-1 border-grey-500 w-full p-x-2 rounded-md text-primary/80">
+    <div className="flex flex-col items-center p-x-2 rounded-md text-primary/80 px-3">
       {servicesList.map((service, key) => {
         return (
           <div
@@ -55,4 +57,49 @@ const ServiceOptions: React.FC<ServicesProps> = ({ services, setServices }) => {
   );
 };
 
-export { ServiceOptions };
+// Car Types
+type CarTypeProps = {
+  carType: string;
+  setCarType: React.Dispatch<React.SetStateAction<string>>;
+};
+const CarTypes: React.FC<CarTypeProps> = ({ carType, setCarType }) => {
+  const carTypesList = [
+    "Crossover(CUV)",
+    "Truck",
+    "Wagon(Estate)",
+    "Sedan(Saloon)",
+    "Hatchback",
+    "Coupe",
+    "Convertible(Roadster)",
+    "Minivan(MPV)",
+  ];
+
+  return (
+    <div className="flex flex-col items-center rounded-md text-primary/80 px-3">
+      <RadioGroup defaultValue="Sedan(Saloon)" className="gap-1 w-full">
+        {carTypesList.map((carType, key) => {
+          return (
+            <div
+              key={`car-type-${key + 1}`}
+              className="flex items-center justify-between w-full hover:bg-secondary rounded-sm p-2"
+            >
+              <label
+                htmlFor={`car-type-${key + 1}`}
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer"
+              >
+                {carType}
+              </label>
+              <RadioGroupItem
+                id={`car-type-${key + 1}`}
+                className="cursor-pointer"
+                value={`${carType}`}
+              />
+            </div>
+          );
+        })}
+      </RadioGroup>
+    </div>
+  );
+};
+
+export { CarTypes, ServiceOptions };
--- frontend/src/components/Filters.tsx
+++ frontend/src/components/Filters.tsx
@@ -5,26 +5,46 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { ServiceOptions } from "@/components/FilterOptions";
+import { CarTypes, ServiceOptions } from "@/components/FilterOptions";
 import { ChevronDown } from "lucide-react";
 
 const Filters = () => {
   const [services, setServices] = useState<string[]>([]);
+  const [carType, setCarType] = useState<string>("");
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button variant="outline">
-          Select an option
-          <span>
-            <ChevronDown />
-          </span>
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="max-h-[50svh] overflow-y-scroll">
-        <ServiceOptions services={services} setServices={setServices} />
-      </PopoverContent>
-    </Popover>
+    <section id="filters" className="flex gap-x-4">
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button variant="outline">
+            Services
+            <span>
+              <ChevronDown />
+            </span>
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="max-h-[50svh] overflow-y-scroll">
+          <div className="h-6 w-full bg-gradient-to-b from-secondary to-transparent sticky top-0" />
+          <ServiceOptions services={services} setServices={setServices} />
+          <div className="h-10 w-full bg-gradient-to-t from-secondary to-transparent sticky bottom-0" />
+        </PopoverContent>
+      </Popover>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button variant="outline">
+            Car Type
+            <span>
+              <ChevronDown />
+            </span>
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="max-h-[50svh] overflow-y-scroll relative">
+          <div className="h-6 w-full bg-gradient-to-b from-secondary to-transparent sticky top-0" />
+          <CarTypes carType={carType} setCarType={setCarType} />
+          <div className="h-10 w-full bg-gradient-to-t from-secondary to-transparent sticky bottom-0" />
+        </PopoverContent>
+      </Popover>
+    </section>
   );
 };
 
