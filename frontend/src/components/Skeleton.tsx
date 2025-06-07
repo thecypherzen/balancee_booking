353c3051af28fa374ea78ded8bc578ecf791aadb
William Inyam <sw.inyam@outlook.com>
2025-06-07 21:42:40 +0000

    :star::up:update:add theme variation to skeleton

--- frontend/src/components/Skeleton.tsx
+++ frontend/src/components/Skeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 const StationSkeleton = () => {
   return (
     <Skeleton>
-      <div className="min-h-[320px] rounded-sm grid shadow-md shadow-primary/5 pb-3 relative bg-white overflow-hidden">
+      <div className="min-h-[320px] rounded-sm grid shadow-md shadow-primary/5 pb-3 relative bg-white dark:bg-zinc-800 overflow-hidden">
         <div className="absolute top-50 right-8 size-20 lg:top-49 lg:right-14 lg:size-26 rounded-full z-10 flex flex-col items-center justify-center bg-secondary outline-2 outline-secondary outline-offset-5">
           <p className="size-4 rounded-full bg-secondary"></p>
         </div>
@@ -32,14 +32,14 @@ const StationSkeleton = () => {
 
             {/* Book Now Form */}
             <div className="mt-10">
-              <div className="bg-secondary gap-2 sp-4 md:p-10 rounded-md flex flex-col gap-4 w-full items-start">
+              <div className="bg-secondary dark:bg-zinc-900 gap-2 sp-4 md:p-10 rounded-md flex flex-col gap-4 w-full items-start">
                 <div className="h-[100px] bg-neutral/10 rounded-md w-4/5"></div>
                 <div className="h-[20px] bg-neutral/10 w-2/5 rounded-md"></div>
               </div>
             </div>
 
             {/* map */}
-            <div className="w-full mt-10 h-[200px] rounded-md bg-neutral/8"></div>
+            <div className="w-full mt-10 h-[200px] rounded-md bg-neutral/8 dark:bg-zinc-900/50"></div>
           </div>
         </div>
       </div>
