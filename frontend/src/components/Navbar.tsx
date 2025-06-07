dbbed9af9951ae3a462bc4786ca2dbaa72a830e9
William Inyam <sw.inyam@outlook.com>
2025-06-07 08:19:11 +0000

    :star2:star:feat:complete theme toggle

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import ContentWrapper from "@/components/ContentWrapper";
 import { useCallback, useEffect, useState } from "react";
 import { useTheme } from "@/hooks/ThemeContext";
+import { Sun, Moon } from "lucide-react";
 
 const Navbar = () => {
   // states
@@ -38,8 +39,17 @@ const Navbar = () => {
     <div
       className={`transition-all duration-500 py-4 shadow-md dark:shadow-xl shadow-black/5 dark:shadow-black/20 bg-white dark:bg-zinc-900 ${isSticky ? "sticky top-0 z-20 text-zinc-900 dark:text-secondary" : ""}`}
     >
-      <ContentWrapper className="flex items-center justify-center">
-        Nabar
+      <ContentWrapper className="flex items-center justify-center gap-2">
+        <div
+          className="shadow-none size-8 text-primary/90 bg-zinc-100 hover:bg-zinc-200 dark:bg-zinc-200 dark:hover:bg-zinc-100 p-[5px] rounded-sm flex flex-col items-center justify-center cursor-pointer"
+          onClick={toggleTheme}
+        >
+          {theme === "light" ? (
+            <Sun strokeWidth="2.2px" size="40px" />
+          ) : (
+            <Moon strokeWidth="2.2px" size="40px" />
+          )}
+        </div>
       </ContentWrapper>
     </div>
   );
