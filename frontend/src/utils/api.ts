895d618d77a3e78cc56b6c5e08d846b315080bf7
William Inyam <sw.inyam@outlook.com>
2025-06-09 08:20:32 +0000

    :up: update api to for production

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const ENV = import.meta.env.ENV;
+
 const api = axios.create({
-  baseURL: "http://localhost:8082",
+  baseURL:
+    ENV === "dev" ? "http://localhost:8082" : import.meta.env.VITE_API_BASE_URL,
 });
 
 export default api;
